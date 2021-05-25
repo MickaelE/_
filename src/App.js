add86a9862ce27232f071f791c73510747b7e319
xmie <mickael.eriksson@scott-tiger.dk>
2021-05-25 15:27:54 +0200

    Added complex https://www.pluralsight.com/guides/how-to-use-reactjs-and-complex-json-objects

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
 //src/App.js
-
-import React, {Component} from 'react';
+// <Registrations registrations={this.state.registrations} />
+import React, {Component,useState} from 'react';
 import Registrations from './components/registrations';
-import { MDBFooter } from 'mdb-react-ui-kit';
-import { appendScript } from './utils/appendScript'
+import { MDBFooter, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from 'mdb-react-ui-kit';
 class App extends Component {
+
     state = {
         registrations: []
     }
-    componentDidMount() {
-        appendScript("header.js");
-        fetch('http://localhost:8080/api/registration/')
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ registrations: data })
-            })
-            .catch(console.log)
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+    componentDidMount(){
     }
     render () {
         return (
-            <div id="parent">
-            <MDBContainer>
-                <Registrations registrations={this.state.registrations} />
-                <MDBBtn>Button</MDBBtn>
 
+            <MDBCard style={{ maxWidth: '22rem' }}>
+                <MDBCardBody>
+                    <MDBCardTitle>Registrations List</MDBCardTitle>
+                    <MDBCardText/>
+                    <Registrations registrations={this.state.registrations} />
+                    <MDBBtn>Skicka</MDBBtn>
 
-            <MDBFooter backgroundColor='light' className='text-center text-lg-left'>
-                <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                    &copy; {new Date().getFullYear()} Copyright:{' '}
-                    <a className='text-dark' href='https://mickenet.com/'>
-                        mickenet.com
-                    </a>
-                </div>
-            </MDBFooter>
-                </MDBContainer>
-    </div>
+                </MDBCardBody>
+                <MDBFooter backgroundcolor='light' className='text-center text-lg-left'>
+                    <div className='text-center p-3' style={{ backgroundcolor: 'rgba(0, 0, 0, 0.2)' }}>
+                        &copy; {new Date().getFullYear()}{' '}
+                        <a className='text-dark' href='https://mickenet.com/'>
+                            mickenet.com
+                        </a>
+                    </div>
+                </MDBFooter>
+            </MDBCard>
         );
     }
 }
-
 export default App;
--- src/components/registrations.js
+++ src/components/registrations.js
@@ -1,16 +1,35 @@
-import React from 'react'
-import { MDBFooter } from 'mdb-react-ui-kit';
+import React, {useEffect,useState} from 'react';
+import axios from 'axios';
 
-const Registrations = ({ registrations: registrations }) => {
+const Registrations = ({ registrations }) => {
+
+    const [Data,setData]=useState({
+        _id:'',
+        Descrlagiption:''
+    })
+    const [colorsData,setColorsData]=useState([])
+    useEffect(()=>{
+        axios.get('http://localhost:8080/api/registration/')
+            .then(res=>{
+                console.log('Response from main API: ',res)
+                console.log('Home Data: ',res.data._id)
+                let companyData=res.data;
+                setData({_id:companyData._id,lag:companyData.lag})
+                console.log('Colors Data: ',res.data.data)
+                setColorsData(res.data.data)
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+    },[])
     return (
         <div>
-            <center><h1>Registrations List</h1></center>
             {registrations.map((registration) => (
                 <div class="card">
                     <div class="card-body">
-                         <h5 class="card-title">{registration._id}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{registration.Lag}</h6>
-                        <p class="card-text">{registration.personalia.namn_barn}</p>
+                         <h5 class="card-title">{Data._id}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{Data.Lag}</h6>
+                        <p class="card-text">{Data.personalia.namn_barn}</p>
 
                     </div>
                 </div>
