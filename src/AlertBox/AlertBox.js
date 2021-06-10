a82b2f8edd70c153f7e3eb4e0e3a0549d5aabfef
xmie <mickael.eriksson@scott-tiger.dk>
2021-06-10 12:26:06 +0200

    Design changes

---
+++ src/AlertBox/AlertBox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+type Props = {}
+
+export default function alertbox({}: Props) {
+    return (<div className="AlertBox"/>);
+}
---
+++ src/MenuComp/MenuComp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+type Props = {}
+
+export default function menucomp({}: Props) {
+    return (<div className="MenuComp"/>);
+}
